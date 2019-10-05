06aac8bc16a71166bb73a455e07c51232460b7a5
israelcamp <israelcampiotti@gmail.com>
2019-10-05 08:51:59 -0300

    first week of cs224n

---
+++ courses/CS224/README.md
@@ -0,0 +1,3 @@
+# Stanford CS224n 2019 - NLP with Deep Learning
+
+- [Course Home Page](https://web.stanford.edu/class/cs224n/index.html#schedule)
---
+++ mlpack/shell_scripts/run_glue.sh
@@ -0,0 +1,20 @@
+export ENV="/home/israel/miniconda3/envs/bertenv/bin/python";
+export SCRIPT="/home/israel/Documents/Mestrado/MLMaster/mlpack/scripts/run_glue.py";
+export GLUE_DIR="/home/israel/Documents/Mestrado/MLMaster/glue_data";
+export TASK_NAME="MRPC"
+
+"${ENV}" "${SCRIPT}" \
+  --model_type bert \
+  --model_name_or_path bert-base-cased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --do_lower_case \
+  --data_dir $GLUE_DIR/$TASK_NAME \
+  --max_seq_length 128 \
+  --per_gpu_train_batch_size 4 \
+  --gradient_accumulation_steps 8 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3.0 \
+  --output_dir /tmp/$TASK_NAME/
+  --fp16
