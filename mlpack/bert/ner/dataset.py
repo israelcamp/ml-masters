484be5b09de9017269aa359bea0fd05b6ab184e2
israelcamp <israelcampiotti@gmail.com>
2019-12-07 20:22:08 -0300

    ner dataset

---
+++ mlpack/bert/ner/dataset.py
@@ -0,0 +1,21 @@
+import torch
+
+
+class FeaturesDataset(Dataset):
+
+    def __init__(self, features):
+        self.features = features
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        raise NotImplementedError
+
+
+class NERDataset(FeaturesDataset):
+
+    def __getitem__(self, idx):
+        feat = self.features[idx]
+        return torch.tensor(feat.input_ids), torch.tensor(feat.input_mask), \
+            torch.tensor(feat.label_ids) - 1, torch.tensor(feat.label_mask)
