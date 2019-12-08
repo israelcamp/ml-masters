75c98c184134c95fb2455666fda871ba8c8c5a81
israelcamp <israelcampiotti@gmail.com>
2019-12-08 16:15:26 -0300

    new model type - bert ner with span

--- mlpack/bert/ner/model.py
+++ mlpack/bert/ner/model.py
@@ -22,28 +22,27 @@ class BertForNERClassification(BertForTokenClassification):
         weights = torch.tensor(weights)
         self.loss_fct = torch.nn.CrossEntropyLoss(weight=weights)
 
+    def _change_bert_grad(self, requires_grad):
+        for name, params in self.model.named_parameters():
+            if name.startswith('bert'):
+                params.requires_grad = requires_grad
+
+    @property
+    def freeze_bert(self):
+        self._change_bert_grad(False)
+
+    @property
+    def unfreeze_bert(self):
+        self._change_bert_grad(True)
+
     def _build_pooler(self, pooler):
+        assert pooler in ('last', 'sum', 'concat')
         if pooler == 'last':
             self.pooler = self.last_layer
-        else:
-            assert pooler == 'sum'
+        elif pooler == 'sum':
             self.pooler = self.sum_last_4_layers
-
-    def _build_classifier(self, config, pooler):
-        """Build tag classifier."""
-        if pooler in ('last', 'sum'):
-            self.classifier = torch.nn.Linear(config.hidden_size,
-                                              config.num_labels)
         else:
-            assert pooler == 'concat'
-            self.classifier = torch.nn.Linear(4 * config.hidden_size,
-                                              config.num_labels)
-
-    def set_bias_tag_O(self, bias_O):
-        # Increase tag "O" bias to produce high probabilities early on and reduce
-        # instability in early training
-        if bias_O is not None:
-            self.classifier.bias.data[0] = bias_O
+            self.pooler = self.concat_last_4_layers
 
     @staticmethod
     def sum_last_4_layers(sequence_outputs):
@@ -65,31 +64,58 @@ class BertForNERClassification(BertForTokenClassification):
         """Simply returns the last tensor of a list of tensors, indexing -1."""
         return sequence_outputs[-1]
 
-    def forward(self, input_ids, input_mask, label_ids, label_mask):
+    @staticmethod
+    def concat_last_4_layers(sequence_outputs):
+        last_layers = sequence_outputs[-4:]
+        return torch.cat(last_layers, dim=-1)
 
+    def _build_classifier(self, config, pooler):
+        """Build tag classifier."""
+        if pooler in ('last', 'sum'):
+            self.classifier = torch.nn.Linear(config.hidden_size,
+                                              config.num_labels)
+        else:
+            assert pooler == 'concat'
+            self.classifier = torch.nn.Linear(4 * config.hidden_size,
+                                              config.num_labels)
+
+    def set_bias_tag_O(self, bias_O):
+        # Increase tag "O" bias to produce high probabilities early on and reduce
+        # instability in early training
+        if bias_O is not None:
+            self.classifier.bias.data[0] = bias_O
+
+    def bert_and_pool(self, input_ids, input_mask):
         _, _, hidden_states = self.bert(input_ids, attention_mask=input_mask)
-
         out = self.pooler(hidden_states)
+        return out
 
+    def forward(self, input_ids, input_mask, label_ids=None, label_mask=None):
+
+        out = self.bert_and_pool(input_ids, input_mask)
         out = self.dropout(out)
         out = self.classifier(out)
-        # take the active logits
-        label_mask = label_mask.view(-1)
-        active_logits = out.view(-1, self.num_labels)[label_mask == 1]
 
-        # take the active labels
-        # remove one because of the [PAD] being the 0 moved to DATASET
-        active_labels = label_ids.view(-1)[label_mask == 1]
+        if label_ids is not None and label_mask is not None:
+            # take the active logits
+            label_mask = label_mask.view(-1)
+            active_logits = out.view(-1, self.num_labels)[label_mask == 1]
 
-        # calc the loss
-        loss = self.loss_fct(active_logits, active_labels)
+            # take the active labels
+            # remove one because of the [PAD] being the 0 moved to DATASET
+            active_labels = label_ids.view(-1)[label_mask == 1]
 
-        return loss, active_logits, active_labels
+            # calc the loss
+            loss = self.loss_fct(active_logits, active_labels)
+
+            return loss, active_logits, active_labels
+
+        return out
 
 
 class BertForMaskedNERClassification(BertForNERClassification):
 
-    def forward(self, input_ids, input_mask, label_ids, label_mask):
+    def forward(self, input_ids, input_mask, label_ids=None, label_mask=None):
 
         _, _, hidden_states = self.bert(input_ids, attention_mask=input_mask)
 
@@ -97,11 +123,35 @@ class BertForMaskedNERClassification(BertForNERClassification):
 
         out = self.dropout(out)
         out = self.classifier(out)
-        # take the active logits
-        label_mask = label_mask.view(-1)
-        active_logits = out.view(-1, self.num_labels)[label_mask == 1]
+        if label_ids is not None and label_mask is not None:
+            # take the active logits
+            label_mask = label_mask.view(-1)
+            active_logits = out.view(-1, self.num_labels)[label_mask == 1]
 
-        # calc the loss
-        loss = self.loss_fct(active_logits, label_ids)
+            # calc the loss
+            loss = self.loss_fct(active_logits, label_ids)
 
-        return loss, active_logits, label_ids
+            return loss, active_logits, label_ids
+        return out
+
+
+class BertForSpanNERClassification(BertForNERClassification):
+
+    def forward(self, input_ids, input_mask, label_ids=None, label_mask=None, max_context=None):
+        out = self.bert_and_pool(input_ids, input_mask)
+        hidden_size = out.shape[-1]
+        if max_context is not None and label_ids is not None and label_mask is not None:
+            _max_context = max_context.view(-1) == 1
+            out = out.view(-1,
+                           hidden_size)[_max_context].view(1, -1, hidden_size)
+            out = self.dropout(out)
+            out = self.classifier(out)
+            # choose labels
+            mask = label_mask.view(-1) == 1
+            active_labels = label_ids.view(-1)[mask * _max_context]
+            active_logits = out.view(-1,
+                                     self.num_labels)[mask[_max_context]]
+            # calc the loss
+            loss = self.loss_fct(active_logits, active_labels)
+            return loss, active_logits, active_labels
+        return out
